b6fa39c9f8ff06fa86dadeb8b71d94d0dcf92826
trylirn <trylirn@gmail.com>
2026-04-07 13:25:37 +0100

    Comment out useState import in EmailSubscriber

--- src/components/EmailSubscriber.tsx
+++ src/components/EmailSubscriber.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/*import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Mail, CheckCircle2 } from "lucide-react";
