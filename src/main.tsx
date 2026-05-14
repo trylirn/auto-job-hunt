34b05c90b2002b927f05f268ebd615b07fe138d3
trylirn <trylirn@gmail.com>
2026-05-14 13:30:12 +0100

    Add service worker unregistration logic
    Unregister old service workers for security cleanup.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+// Force-unregister any old service workers (security cleanup)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((reg) => reg.unregister());
+  });
+}
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
