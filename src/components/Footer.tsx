029b2660ce3db6d2a29fa866d011738af99a598b
trylirn <trylirn@gmail.com>
2026-06-25 14:06:42 +0100

    Remove saasfame.com badge from footer
    Removed link and image for saasfame.com from the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -191,17 +191,6 @@ export function Footer() {
                   width="200" 
                   height="54"
                 />
-              </a> 
-              <a 
-                href="https://saasfame.com/item/eplicant"
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <img 
-                  src="https://saasfame.com/badge-light.svg" 
-                  alt="Featured on saasfame.com" 
-                  style="height: 54px; width: auto;" 
-                />
               </a>
             </div>
           </div>
