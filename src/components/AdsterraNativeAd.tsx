30dd352284b77de64958d85257c681d2d95d7392
trylirn <trylirn@gmail.com>
2026-08-12 11:20:53 +0100

    Add AdsterraNativeAd component with ad script

---
+++ src/components/AdsterraNativeAd.tsx
@@ -0,0 +1,2 @@
+<script async="async" data-cfasync="false" src="https://pl30810432.effectivecpmnetwork.com/73fe361683f8d7e9cebdfe096c2ade98/invoke.js"></script>
+<div id="container-73fe361683f8d7e9cebdfe096c2ade98"></div>
